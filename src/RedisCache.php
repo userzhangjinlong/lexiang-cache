aa74162350b9c1bd14e8b93a12e3785ec2fc8b91
userzhangjinlong <825188432@qq.com>
2020-02-06 09:58:03 +0800

    安装扩展包提交

---
+++ src/RedisCache.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zjl
+ * Date: 20-2-5
+ * Time: 下午5:47
+ */
+
+class RedisCache{
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (!self::$instance instanceof self)
+        {
+            static::$instance = new self();
+        }
+
+        return static::$instance;
+    }
+
+    private function __construct()
+    {
+        $redis = new Client();
+    }
+
+    private function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    private function __wakeup()
+    {
+        // TODO: Implement __wakeup() method.
+    }
+
+}
