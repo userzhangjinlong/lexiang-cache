857486b511b4d92e43af985aa3b77e6621398660
userzhangjinlong <825188432@qq.com>
2020-02-06 11:13:51 +0800

    实例花redis单列实例

--- src/RedisCache.php
+++ src/RedisCache.php
@@ -7,8 +7,11 @@
  */
 namespace Lexiang;
 
+use Predis\Client;
+
 class RedisCache{
     private static $instance;
+    private static $config;
 
     public static function getInstance()
     {
@@ -22,7 +25,27 @@ class RedisCache{
 
     private function __construct()
     {
-        $redis = new Client();
+        $config = self::getConfig();
+
+        $redis = new Client($config);
+
+        return $redis;
+
+    }
+
+    /**
+     * @return array
+     */
+    private static function getConfig()
+    {
+        if (static::$config) return self::$config;
+
+        self::$config = [
+            'host'  =>  ENV('REDIS_HOST') ?: '127.0.0.1',
+            'port'  =>  ENV('REDIS_PORT') ?: '6379'
+        ];
+
+        return self::$config;
     }
 
     private function __clone()
